4c8ad34cd3e014d6d28865dcca5bdad3dffe6891
Manoj Kumar <manojsfstm5@gmail.com>
2023-02-08 10:52:18 +0530

    done

--- index.js
+++ index.js
@@ -5,3 +5,13 @@
   backSpeed: 60,
   loop: true,
 });
+
+let drive=document.querySelector(".Drivegoogle")
+drive.addEventListener("click",openDrive);
+
+
+function openDrive(){
+  window.open(
+    (href="https://drive.google.com/file/d/10C5IVHqeM2huu0F-KyZHNAo3XD0VGp9w/view?usp=share_link")
+  );
+}
