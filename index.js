20066ba4612a9ecf5627887a9cbbb13fd4045bdf
Manoj Kumar <manojsfstm5@gmail.com>
2023-04-25 22:49:37 +0530

    project and contact section updated

--- index.js
+++ index.js
@@ -53,50 +53,50 @@ function opendrive(){
 // sr.reveal('#resume-button-2',{interval:500})
 
 
-const sr = ScrollReveal({
-  origin:'left',
-  distance:'120px',
-  duration:2000,
-  reset:true
-})             
+// const sr = ScrollReveal({
+//   origin:'left',
+//   distance:'120px',
+//   duration:2000,
+//   reset:true
+// })             
 
-   // scroll HOME//
-  sr.reveal('#home',{})
-  sr.reveal('#one',{delay:300})
-  sr.reveal('.home-img',{delay:400})
-  sr.reveal('#user-detail-name',{delay:500})
-  sr.reveal('.text-1',{delay:600})
-  sr.reveal('#resume-button-2',{interval:700})
+//    // scroll HOME//
+//   sr.reveal('#home',{})
+//   sr.reveal('#one',{delay:300})
+//   sr.reveal('.home-img',{delay:400})
+//   sr.reveal('#user-detail-name',{delay:500})
+//   sr.reveal('.text-1',{delay:600})
+//   sr.reveal('#resume-button-2',{interval:700})
 
-  sr.reveal('#about h1',{delay:200})
-  sr.reveal('#img',{delay:400})
-  sr.reveal('#user-detail-intro',{delay:600})
+//   sr.reveal('#about h1',{delay:200})
+//   sr.reveal('#img',{delay:400})
+//   sr.reveal('#user-detail-intro',{delay:600})
 
-  // scroll SKILLS//
-  sr.reveal('.skill',{delay:200})
-
-
-       // scroll PROJECTS//
-       sr.reveal('.project',{})
-       sr.reveal('.project-card img',{delay:100})
-       sr.reveal('.project-title',{delay:200})
-       sr.reveal('.project-description',{delay:200})
-       sr.reveal('.project-tech-stack',{delay:200})
-       sr.reveal('.repo-links',{delay:200})
+//   // scroll SKILLS//
+//   sr.reveal('.skill',{delay:200})
 
 
-         // scroll GITHUB//
-    sr.reveal('#github-streak-stats',{delay:200})
-    sr.reveal('#github-top-langs',{delay:400})
-    sr.reveal('#github-stats-card',{delay:600})
+//        // scroll PROJECTS//
+//        sr.reveal('.project',{})
+//        sr.reveal('.project-card img',{delay:100})
+//        sr.reveal('.project-title',{delay:200})
+//        sr.reveal('.project-description',{delay:200})
+//        sr.reveal('.project-tech-stack',{delay:200})
+//        sr.reveal('.repo-links',{delay:200})
 
-    sr.reveal('.calendar',{delay:600})
+
+//          // scroll GITHUB//
+//     sr.reveal('#github-streak-stats',{delay:200})
+//     sr.reveal('#github-top-langs',{delay:400})
+//     sr.reveal('#github-stats-card',{delay:600})
+
+//     sr.reveal('.calendar',{delay:600})
 
 
 
-     // scroll CONTACT//
-     sr.reveal('.column-left',{})   
-     sr.reveal('#contact-github',{delay:100})
-     sr.reveal('#contact-linkedin',{delay:200}) 
-     sr.reveal('#contact-phone',{delay:300}) 
-     sr.reveal('#contact-email',{delay:400})
+//      // scroll CONTACT//
+//      sr.reveal('.column-left',{})   
+//      sr.reveal('#contact-github',{delay:100})
+//      sr.reveal('#contact-linkedin',{delay:200}) 
+//      sr.reveal('#contact-phone',{delay:300}) 
+//      sr.reveal('#contact-email',{delay:400})
